5eb786c67f9d4853175b308131efc50fc6083244
Thiago Almeida <thiagoduminellidealmeida@hotmail.com>
2021-09-02 14:52:47 -0300

    Create exercicio29.py

---
+++ exercicio29.py
@@ -0,0 +1,6 @@
+n1 = float(input("Digite o valor da primeira nota: "))
+n2 = float(input("Digite o valor da segunda nota: "))
+n3 = float(input("Digite o valor da terceira nota: "))
+n4 = float(input("Digite o valor da quarta nota: "))
+media = (n1+n2+n3+n4)/4
+print("A média das notas {}, {}, {}, {} ficou em {}".format(n1, n2, n3, n4, media))
